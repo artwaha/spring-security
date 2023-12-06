e48dde5ab798a489002d9e09fc14408482ea1bea
nasa <appsnasa57@gmail.com>
2023-12-06 04:11:17 -0800

    made some change to the patient update mobile

---
+++ src/main/java/orci/or/tz/appointments/dto/patient/PatientUpdateMobileDto.java
@@ -0,0 +1,10 @@
+package orci.or.tz.appointments.dto.patient;
+
+import lombok.Data;
+
+@Data
+public class PatientUpdateMobileDto {
+    private String mobile;
+    private String registrationNumber;
+}
+
--- src/main/java/orci/or/tz/appointments/web/api/PatientApi.java
+++ src/main/java/orci/or/tz/appointments/web/api/PatientApi.java
@@ -1,11 +1,14 @@
 package orci.or.tz.appointments.web.api;
 
+import orci.or.tz.appointments.dto.patient.PatientUpdateMobileDto;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import orci.or.tz.appointments.dto.patient.PatientDto;
 import orci.or.tz.appointments.exceptions.OperationFailedException;
 import orci.or.tz.appointments.exceptions.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,4 +22,8 @@ public interface PatientApi {
     @ApiOperation(value = "View All Patient By Registration Number", notes = "View All Patient By Registration Number")
     @RequestMapping(value = "/regno", method = RequestMethod.GET, produces = "application/json")
     ResponseEntity<PatientDto> GetPatientByRegistration(@RequestParam String regNo) throws ResourceNotFoundException, IOException, OperationFailedException;
+
+    @ApiOperation(value = "Enable the patient to updatwe the mobile Number", notes = "Enable the patient to updatwe the mobile Number")
+    @RequestMapping(value = "/mobile", method = RequestMethod.PUT, produces = "application/json")
+    ResponseEntity<PatientDto> UpdatePatientMobileNumber(@RequestBody PatientUpdateMobileDto patientUpdateMobileDto) throws ResourceNotFoundException, IOException;
 }
