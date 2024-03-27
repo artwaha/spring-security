018a2aa17995b7462ae5676e0d45e9c404fc4d09
Abdul-razak Twaha <abdul-razak.twaha@outlook.com>
2024-03-27 13:04:31 +0300

    first commit

---
+++ springraphql/src/test/java/com/artwaha/springraphql/SpringBootGraphQlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artwaha.springraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraphQlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
