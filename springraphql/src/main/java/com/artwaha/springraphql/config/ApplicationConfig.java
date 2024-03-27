f5d86a345eb25b29b06918903b7059cde3406abb
Abdul-razak Twaha <abdul-razak.twaha@outlook.com>
2024-03-27 13:44:47 +0300

    updates

---
+++ springraphql/src/main/java/com/artwaha/springraphql/config/ApplicationConfig.java
@@ -0,0 +1,15 @@
+package com.artwaha.springraphql.config;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
