32785ddc5e77724b9b9c5c5da48558decea4e1d4
Abdul-razak Twaha <abdul-razak.twaha@outlook.com>
2024-03-30 14:20:16 +0300

    Refactoring

--- src/main/java/com/atwaha/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/atwaha/springsecurity/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                             .authenticated();
                 }
         );
-        http.userDetailsService(userDetailsService);
+//        http.userDetailsService(userDetailsService);
         http.sessionManagement(session -> {
             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         });
